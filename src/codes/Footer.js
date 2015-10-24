2e635e63c1dced489617cc9d4a2abec56dbce71c
ElvisKang <elviskang@foxmail.com>
2015-10-24 23:42:50 +0800

    modified Footer

--- src/codes/Footer.js
+++ src/codes/Footer.js
@@ -1,22 +1,20 @@
 let demo1={
     code:`
-<md-footer>
-    <md-footer-content slot="main" :options="demo1.content"></md-footer-content>
-    <md-footer-links slot="main" :options="demo1.links"></md-footer-links>
+<md-footer class="green">
+    <md-footer-content slot="main" class="col s12 l6" :options="demo1.content"></md-footer-content>
+    <md-footer-links slot="main" class="col s12 l6" :options="demo1.links"></md-footer-links>
     <md-footer-copyright slot="copyright" :options="demo1.copr"></md-footer-copyright>
 </md-footer>`,
 
     options:`
 demo1:{
         content:{
-            col:'s12 l6',
             title:'Title1',
             text:'this is content1',
             titleClass:"white-text",
             textClass:"white-text"
         },
         links:{
-            col:'s12 l6',
             title:'Links',
             links:[{href:"#!/footer",text:"Footer"}],
             titleClass:"white-text",
@@ -35,18 +33,18 @@ demo1:{
 
 let demo2={
     code:`
-<md-footer>
-        <md-footer-content slot="main" :options="{col:'l4 s12'}">
+<md-footer class="red">
+        <md-footer-content slot="main" class="col s12 l4">
             <p>Another Content</p>
             <h5 slot="title">Title1</h5>
             <p slot="text" class="">Content</p>
-            <md-btn class="red" >Code2</md-btn>
+            <md-btn class="yellow" >Code2</md-btn>
         </md-footer-content>
-        <md-footer-content slot="main" :options="{col:'l4 s12'}">
+        <md-footer-content slot="main" class="col s12 l4">
             <h5 slot="title">Title2</h5>
             <p slot="text" class="">Content2</p>
         </md-footer-content>
-        <md-footer-links slot="main"  :options="{col:'l4 s12'}">
+        <md-footer-links slot="main"  class="col s12 l4">
             <h5 slot="title" class="white-text">Title3</h5>
             <ul slot="links">
                 <li><a href="#!/footer"  class="white-text">Footer</a></li>                
--- src/views/Footer.vue
+++ src/views/Footer.vue
@@ -8,9 +8,9 @@
     <section>
     <doc-item-title>Basic</doc-item-title> 
         <doc-item-demo>
-            <md-footer>
-                <md-footer-content slot="main" :options="demo1.content"></md-footer-content>
-                <md-footer-links slot="main" :options="demo1.links"></md-footer-links>
+            <md-footer class="green">
+                <md-footer-content slot="main" class="col s12 l6" :options="demo1.content"></md-footer-content>
+                <md-footer-links slot="main" class="col s12 l6" :options="demo1.links"></md-footer-links>
                 <md-footer-copyright slot="copyright" :options="demo1.copr"></md-footer-copyright>
             </md-footer>
         </doc-item-demo>
@@ -24,18 +24,18 @@
     <section>
     <doc-item-title>Customized with Slot</doc-item-title>
         <doc-item-demo>
-            <md-footer>
-                <md-footer-content slot="main" :options="{col:'l4 s12'}">
+            <md-footer class="red">
+                <md-footer-content slot="main" class="col s12 l4">
                         <p>Another Content</p>
                         <h5 slot="title">Title1</h5>
                         <p slot="text" class="">Content</p>
-                        <md-btn class="red" >Code2</md-btn>
+                        <md-btn class="yellow" >Code2</md-btn>
                 </md-footer-content>
-                <md-footer-content slot="main" :options="{col:'l4 s12'}">
+                <md-footer-content slot="main" class="col s12 l4">
                     <h5 slot="title">Title2</h5>
                     <p slot="text" class="">Content2</p>
                 </md-footer-content>
-                <md-footer-links slot="main"  :options="{col:'l4 s12'}">
+                <md-footer-links slot="main"  class="col s12 l4">
                     <h5 slot="title" class="white-text">Title3</h5>
                     <ul slot="links">
                         <li><a href="#!/footer"  class="white-text">Footer</a></li>                
@@ -69,14 +69,12 @@
                 codes,
                 demo1:{
                     content:{
-                        col:'s12 l6',
                         title:'Title1',
                         text:'this is content1',
                         titleClass:"white-text",
                         textClass:"white-text"
                     },
                     links:{
-                        col:'s12 l6',
                         title:'Links',
                         links:[{href:"#!/footer",text:"Footer"}],
                         titleClass:"white-text",
@@ -95,9 +93,3 @@
         }
     }
 </script>
-
-<style>
-    #footer footer {
-        background-color: #41b883;
-    }
-</style>
