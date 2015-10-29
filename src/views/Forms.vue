2d5cf70e8f448a5ed976b815ffffedb07303fc23
ElvisKang <elviskang@foxmail.com>
2015-10-29 22:55:06 +0800

    md-branch page

--- src/views/Forms.vue
+++ src/views/Forms.vue
@@ -141,17 +141,17 @@
                     <md-input class="col s12 l6" v-md-model="bindings.firstname" name="firstname" id="firstname" >First Name</md-input>
                     <md-input class="col s12 l6" v-md-model="bindings.lastname" name="lastname"  id="lastname" >Last Name</md-input>
             </div>
-<!--             <div class="row">
-                    <md-radio v-md-model="bindings.sex" value="man" id="man" name="sex" @click="change('msg')">Man</md-radio>
-                    <md-radio v-md-model="bindings.sex" value="Woman" id="woman" name="sex">Woman</md-radio>
-            </div> -->
+            <div class="row">
+                    <md-radio v-md-model="bindings.sex" value="man" id="man" name="sex">Man</md-radio>
+                    <md-radio v-md-model="bindings.sex" value="woman" id="woman" name="sex">Woman</md-radio>
+            </div>
             <div class="row">
                 <md-textarea class="col s12" v-md-model="bindings.words" icon-text="mode_edit" id="words" placeholder="Say something..." >Textarea</md-input>
             </div>
             <div class="row">
                 <div class="col s12">
                     <p>Hello! {{bindings.firstname}} {{bindings.lastname}}</p>
-                    <!-- <p>You are a {{bindings.sex}}</p> -->
+                    <p>You are a {{bindings.sex}}</p>
                     <p>You said: {{bindings.words}}</p>
                 </div>
             </div>
@@ -169,9 +169,6 @@
         data(){
             return {
                 bindings:{
-                    firstname:null,
-                    lastname:null,
-                    words:null
                 },
                 bool:true,
                 logo:{
@@ -180,11 +177,6 @@
                     pos:"center"
                 },
                 codes
-            }
-        },
-        methods:{
-            change(){
-                console.log("changed");
             }
         }
     }
